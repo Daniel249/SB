5e12cefd3ef5bc93316f97af84cc0ee54d495159
Daniel <danielmorillo249@hotmail.com>
2018-01-18 01:49:55 +0100

    added run loop and constructor

--- battle.cs
+++ battle.cs
@@ -2,12 +2,40 @@
 public class Battle {
     Map map;
     Queue queue;
+    Player player;
     
     // get set
-    public Queue GetQueue() {
+    public Queue getQueue() {
         return queue;
     }
-    public Map GetMap() {
+    public Map getMap() {
         return map;
+    }
+    // run battle
+    public bool turn() {
+        return queue.run();
+    }
+    public void run() {
+        while(true) {
+            if(!Game.getContinueGame()) {
+                endGame();
+                break;
+            }
+            // run player and AI turn
+            player.runTurn();
+            Game.runTurn();
+
+            turn();
+        }
+    }
+    void endGame() {
+
+    }
+    // constructor
+    public Battle() {
+        player = new Player();
+        map = new Map(100, 60, 5, 5);
+        queue = new Queue();
+        
     }
 }
