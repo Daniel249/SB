1d2354ecb41b2d6fcf2f2591128f9923129ed640
Daniel <danielmorillo249@hotmail.com>
2018-01-18 01:56:05 +0100

    added units queue

--- queue.cs
+++ queue.cs
@@ -1,4 +1,15 @@
-
+using System.Collections.Generic;
 public class Queue {
+    List<Thing> unitsQueue;
+    public bool run() {
+        foreach(Thing thi in unitsQueue) {
+            thi.turn();
+        }
+        return true;
+    }
     
+    // constructor
+    public Queue() {
+        unitsQueue = new List<Thing>();
+    }
 }
