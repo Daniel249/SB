aeb09d383113fa8b4a3af25b6d0b8f8e15d3f68d
Daniel <danielmorillo249@hotmail.com>
2018-01-21 21:24:45 +0100

    added default console colors. buffer size changed with window size

--- console.cs
+++ console.cs
@@ -10,12 +10,15 @@ static class Terminal {
     public static int getSize_y() {
         return size_y;
     }
+    // set console size
     public static void setSize(int sizex, int sizey) {
         size_x = sizex;
         size_y = sizey;
-        Console.CursorVisible = false;
         // set buffer
         Console.SetWindowSize(size_x, size_y);
+        Console.SetBufferSize(size_x, size_y);
+        Console.Clear();
+        Console.CursorVisible = false;
     }
     public static void PrintChar(char cha, int pos_x, int pos_y, ConsoleColor bcolor, ConsoleColor fcolor) {
         Console.SetCursorPosition(pos_x, pos_y);
@@ -23,5 +26,15 @@ static class Terminal {
         Console.BackgroundColor = bcolor;
         Console.Write(cha);
         Console.ResetColor();
+    }
+    // default console colors
+    static ConsoleColor bcolor = ConsoleColor.Black;
+    static ConsoleColor fcolor = ConsoleColor.White;
+    // get set
+    public static ConsoleColor getDefaultBack() {
+        return bcolor;
+    }
+    public static ConsoleColor getDefaultFore() {
+        return fcolor;
     }
 }
