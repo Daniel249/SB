9afa46cd9442790119a1ad20afb606930295b53a
Daniel <danielmorillo249@hotmail.com>
2018-01-25 15:51:12 +0100

    added firing logic and loading to unit on constructor

--- weapon.cs
+++ weapon.cs
@@ -1,4 +1,35 @@
 
 public class Weapon {
-    
+    // position in unit
+    int position_x;
+    int position_y;
+    // reference to unit
+    Unit ship;
+    int attackDamage;
+    // attackSpeed mechanic
+    Cronometer cronometer;
+    // spawn bullet. can be accessed directly altough build for checkFire with cronometer
+    public void fire() {
+        Bullet bl = new Bullet(position_x + ship.getPos_x(), position_y + ship.getPos_y());
+        // attackDamage = 5;
+    }
+    // check cronometer to fire
+    public void checkFire() {
+        if(cronometer.tick()) {
+            fire();
+        }
+    }
+    public static void loadUnit(Unit u, int AS) {
+        Weapon w = new Weapon(u.getCode().GetLength(0), 0, AS);
+        u.setWeapon(w);
+        w.ship = u;
+    }
+    public Weapon(int pos_x, int pos_y, int AS) {
+        Game.getQueue().weaponQueue.Add(this);
+        position_x = pos_x;
+        position_y = pos_y;
+        cronometer = new Cronometer(AS);
+    }
 }
+
+// types of weapons
