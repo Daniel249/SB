f062ab6fa4e20b48c4a8e234680ed63fed6d3e09
Daniel <danielmorillo249@hotmail.com>
2018-01-29 00:18:38 +0100

    test randomly generated units

--- Program.cs
+++ Program.cs
@@ -12,12 +12,25 @@ class Program {
             Printer.printThing(thi);
         }
         // remove test
+        test(5);
         bat.run();
-
 
         // test reader
         // Filereader f = new Filereader("./textures.txt");
         // f.printTexture("main");
+    }
+    public static void test(int num) {
+        Random generator = new Random();
+        int limit_x = Game.getMap().getSize_x() - 5;
+        int limit_y = Game.getMap().getSize_y() - 5;
+
+        for(int i = 0; i < num; i++) {
+            int x = generator.Next(0, limit_x);
+            int y = generator.Next(0, limit_y);
+
+            Unit u = new Unit(x, y, 1);
+            Game.getBattle().getList().Add(u);
+        }
     }
 }
 
