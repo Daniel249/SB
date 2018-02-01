04c06bd066ab894b30a2fccb6990803e49e5ac4b
Daniel <danielmorillo249@hotmail.com>
2018-02-01 12:37:59 +0100

    moved test methods to library

--- Program.cs
+++ Program.cs
@@ -8,34 +8,12 @@ static class Program {
         Terminal.setSize();
         Game.setUp();
         Battle bat = new Battle(Terminal.getSize_x() - 75, Terminal.getSize_y() - 10);
-        test(5);
-        Terminal.PrintString("move: up down keys, left key to stop    toggle fire: F    exit: esc", 10, 0, ConsoleColor.Black, ConsoleColor.White);
+        Test.test(5);
         bat.run();
 
         // test reader
         // Filereader f = new Filereader("./textures.txt");
         // f.printTexture("main");
-    }
-
-    
-    // used on Main
-    public static void test(int num) {
-        for(int i = 0; i < num; i++) {
-            spawn();
-        }
-    }
-    // used on test and on unit death
-    static Random generator = new Random();
-    public static void spawn() {
-        
-        int limit_x = Game.getMap().getSize_x() - 5;
-        int limit_y = Game.getMap().getSize_y() - 5;
-
-        int x = generator.Next(10, limit_x);
-        int y = generator.Next(0, limit_y);
-
-        Unit u = new Unit(x, y, 1, false);
-        Weapon.loadUnit(u, 0, 45, false);
     }
 }
 
--- library.cs
+++ library.cs
@@ -28,4 +28,25 @@ static class Test {
         }
         return testCode;
     }
+
+    // minigame. spawn 5 enemies
+        // used on Main
+    public static void test(int num) {
+        for(int i = 0; i < num; i++) {
+            spawn();
+        }
+    }
+    // used on test and on unit death
+    static Random generator = new Random();
+    public static void spawn() {
+        
+        int limit_x = Game.getMap().getSize_x() - 5;
+        int limit_y = Game.getMap().getSize_y() - 5;
+
+        int x = generator.Next(10, limit_x);
+        int y = generator.Next(0, limit_y);
+
+        Unit u = new Unit(x, y, 1, false, "minor");
+        Weapon.loadUnit(u, 0, 45, false);
+    }
 }
