0277b1a7fd09141d9c711d9101a5010588740abc
Daniel <danielmorillo249@hotmail.com>
2018-02-09 19:31:58 +0100

    added namespace

--- Program.cs
+++ Program.cs
@@ -1,16 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
+﻿
 
+namespace SB 
+{
+    static class Program {
+        static void Main(string[] args) {
 
-static class Program {
-    static void Main(string[] args) {
-
-        Terminal.setSize();
-        Game.setUp();
-        
-        Battle bat = new Battle(Terminal.getSize_x() - 75, Terminal.getSize_y() - 10);
-        Test.check();
-        bat.run();
+            Terminal.setSize();
+            Game.setUp();
+            
+            Battle bat = new Battle(Terminal.getSize_x() - 75, Terminal.getSize_y() - 10);
+            Test.check();
+            bat.run();
+        }
     }
+    
 }
 
