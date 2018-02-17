ce3ab58d733d04626c4a65306a7b1615f8283ec4
Daniel <danielmorillo249@hotmail.com>
2018-02-17 16:38:33 +0100

    map inherits generic 2d array

--- source/Scheme/map.cs
+++ source/Scheme/map.cs
@@ -1,32 +1,36 @@
 using System;
 using System.Collections.Generic;
 using SB.Objects;
+using GameLibrary.Graphics;
 using GameLibrary.Services;
 
 namespace SB {
 
-public class Map {
-    // map size
-    // readonly
-    public readonly int Size_x;
-    public readonly int Size_y;
-
-
+public class Map : ISnapshot<Entity> {
     // map location
     // readonly
     public readonly int Position_x;
     public readonly int Position_y;
 
 
+    // ISnapshot implementation 
+
+    // map size
+    // readonly
+    public int Size_x { get; private set; }
+    public int Size_y { get; private set; }
+
     // 2d array first dimension y. second dimension x
-    readonly Entity[][] mapp;
+    public Entity[][] state { get; private set; }
+
+
 
     // get unit in map
     public Entity getMap(int pos_x, int pos_y) {
         if(!checkPosition(pos_x, pos_y)) {
             return null;
         }
-        return mapp[pos_y][pos_x];
+        return state[pos_y][pos_x];
     }
 
     // set unit in map
@@ -34,7 +38,7 @@ public class Map {
         if(!checkPosition(pos_x, pos_y)) {
             return;
         }
-        mapp[pos_y][pos_x] = u;
+        state[pos_y][pos_x] = u;
     }
 
 
@@ -58,7 +62,7 @@ public class Map {
         Size_y = _size_y;
         Position_x = loc_x;
         Position_y = loc_y;
-        mapp = MapInitializer.CreateArray<Entity>(Size_x, Size_y);
+        state = TextureInitializer.CreateArray<Entity>(Size_x, Size_y);
     }
 }
 }
