bd47140d8a27e123094dc99b1b869dce3399c2c0
Daniel <danielmorillo249@hotmail.com>
2018-02-17 20:46:35 +0100

    improvised switch interface and printer types

--- source/Program.cs
+++ source/Program.cs
@@ -1,6 +1,7 @@
 ﻿using GameLibrary.Graphics.Display;
 using GameLibrary.Graphics;
 using GameLibrary.Platform;
+using GameLibrary.Interface;
 
 namespace SB {
 static class Program {
@@ -8,15 +9,29 @@ static class Program {
 
         Terminal.setSize();
         Game.setUp();
+
+        // define printer in main screen
+        // Printer MainPrinter = new LegacyPrinter();
+        Printer MainPrinter = new LinePrinter();
+        
         Game.setScreen(
             new Screen(Terminal.Size_x - 75, Terminal.Size_y - 10, 
-                new LegacyPrinter())
+                MainPrinter)
         );
         
         Battle bat = new Battle(Terminal.Size_x - 75, Terminal.Size_y - 10);
+
+        // set GUI. choose one based on LegacyPrinter or standard
+        gUInterface = new MapInterface(Game.getMap(), Game.getMainScreen(), 0, 0, Terminal.Size_x - 75, Terminal.Size_y - 10);
+        // gUInterface = new LegacyInterface(Game.getMap(), Game.getMainScreen(), 0, 0, Terminal.Size_x - 75, Terminal.Size_y - 10);
+
+        bat.guinterface = gUInterface;
+        bat.setPlayer(new Player(true));
+
         Test.check();
         bat.run();
     }
+    public static GUInterface gUInterface;
 }
     
 }
